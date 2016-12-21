5de24b1dba344e336fb4f268e14935332057c698
Volodymyr Manko <vmanko1@gmail.com>
2016-12-21 03:54:07 +0200

    testing.

--- spd-university/mm-rwd-bem/blocks/feedback-list/enlargePhoto.js
+++ spd-university/mm-rwd-bem/blocks/feedback-list/enlargePhoto.js
@@ -1,24 +1,28 @@
 (function () {
-    let img = document.createElement('img'),
+    var img = document.createElement('img'),
         shadowField = document.createElement('div'),
         support = document.getElementById('support');
 
     support.onclick = function (event) {
-        let target = event.target;
+        var target = event.target;
 
-        if (window.matchMedia("(min-width: 480px)").matches) {
+        if (target.className.indexOf('feedback-list__avatar') !== -1 || target.className.indexOf('feedback-list__link') !== -1) {
+            event.preventDefault();
 
-            if (target.className.indexOf('feedback__avatar') !== -1 || target.className.indexOf('feedback__avatar-link') !== -1) {
-                event.preventDefault();
+            if (!window.matchMedia) getFullPhoto(target.parentNode.href); // for IE9...
+
+
+            if (window.matchMedia("(min-width: 480px)").matches) {
                 if (target.tagName !== 'IMG') getFullPhoto(target.href);
-            }
-        } else event.preventDefault();
+            } else event.preventDefault();
+        }
     };
 
     function getFullPhoto(url) {
         shadowField.className = 'lock-screen';
-        img.className = 'align-img';
+        img.className = 'align-zoomed-img';
         img.src = url;
+
         shadowField.appendChild(img);
         document.body.appendChild(shadowField);
         shadowField.onclick = function() { closeImg(); }
