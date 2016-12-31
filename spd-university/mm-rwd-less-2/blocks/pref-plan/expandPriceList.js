e2a5b15e918abf68be83d3d2f3010ede7c14a3d3
Volodymyr Manko <vmanko1@gmail.com>
2016-12-31 15:02:19 +0200

    testing rwd 2.0 (images optimization)

---
+++ spd-university/mm-rwd-less-2/blocks/pref-plan/expandPriceList.js
@@ -0,0 +1,46 @@
+(function () {
+    var prefPlans = document.querySelectorAll('.pref-plan__content'),
+        priceList = document.getElementsByClassName('price-list')[0],
+        prefPlanEx = document.querySelector('.pref-plan__title_exclusive'),
+        widthForAccordion = '(max-width: 420px)';
+
+    triggerAccordion(widthForAccordion);
+
+    window.addEventListener('resize', function() {
+        triggerAccordion(widthForAccordion);
+    });
+
+    function triggerAccordion(media) {
+        if (window.matchMedia(media).matches) {
+            expandList(prefPlanEx);
+
+            priceList.addEventListener('click', expandListIfTitleClicked);
+        } else {
+            priceList.removeEventListener('click', expandListIfTitleClicked);
+            for (var i = 0; i < prefPlans.length; i++) {
+                if (prefPlans[i].hasAttribute('hidden'))
+                    prefPlans[i].removeAttribute('hidden');
+            }
+        }
+    }
+
+    function expandListIfTitleClicked(event) {
+        var target = event.target;
+
+        if (target.className.indexOf('pref-plan__title') === -1) return;
+        expandList(target);
+    }
+
+    function expandList(el) {
+        for (var i = 0; i < prefPlans.length; i++) {
+            if (prefPlans[i] === el.nextElementSibling) continue;
+            prefPlans[i].setAttribute('hidden', '');
+            prefPlans[i].previousElementSibling.setAttribute('left-pointer', '\u25C0'); // ◄
+        }
+
+        if (el.nextElementSibling.hasAttribute('hidden')) {
+            el.nextElementSibling.removeAttribute('hidden');
+            el.removeAttribute('left-pointer');
+        }
+    }
+})();
